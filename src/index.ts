f67ea6228dcc56594dc907f1195a07f8c5b8fbc2
alfianardhi <ardhi.alfian@gmail.com>
2021-01-20 01:33:24 +0700

    create route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hi you...");
+import express, {Application, Request, Response} from "express";
+
+
+class App {
+    public app:Application;
+    constructor(){
+        this.app = express();
+        this.routes();
+    }
+
+    private routes(): void {
+        this.app.route('/').get((req:Request, res:Response) => {
+            res.send("hello rest api with typescript");
+        });
+    }
+}
+
+const app = new App().app;
+const port: number = 3000;
+
+app.listen(port, () => {
+    console.log(`This app listening at http://localhost:${port}`);
+
+    //console.log(`This app run is ${process.env.DB_HOST}`);
+});
